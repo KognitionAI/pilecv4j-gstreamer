572120fd0a54a1c614c0c326ab125f868052c085
Jim Carroll <jim@dontcallme.com>
2022-07-18 09:39:21 -0400

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+## What is this project?
+
+This is the deprecated [GStreamer](https://gstreamer.freedesktop.org/) based functionality previously part of [pilecv4j](https://github.com/KognitionAI/pilecv4j). All of this functionality is now better implemented on top of [FFMpeg](https://ffmpeg.org/) in pilecv4j's lib-ffmpeg.
---
+++ native-common-include/include/imagemaker.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <cstdint>
+
+#include <stdlib.h>
+#include <stdint.h>
+
+namespace ai {
+  namespace kognition {
+    namespace pilecv4j {
+
+      struct MatAndData {
+        uint64_t mat;
+        void* data;
+      };
+
+      /**
+       * This coresponds to various CV_XXXX formats.
+       */
+      enum PixelFormat {
+        UNKNOWN,
+        RGB24,
+        BGR24
+      };
+
+      struct RawRaster {
+        uint8_t* data;
+        PixelFormat pixFormat;
+        int w;
+        int h;
+        size_t stride; // bytes per row
+      };
+
+      class ImageMaker {
+      public:
+        virtual ~ImageMaker() = default;
+
+        virtual uint64_t makeImage(int height, int width, int stride, void* data) = 0;
+
+        virtual MatAndData allocateImage(int height, int width) = 0;
+
+        virtual uint64_t allocateImageWithCopyOfData(int height, int width, int stride, void* data) = 0;
+
+        virtual void freeImage(uint64_t mat) = 0;
+
+        virtual uint64_t copy(uint64_t mat) = 0;
+
+        virtual bool extractImageDetails(uint64_t matRef, bool isRgb, RawRaster* rasterToFill) = 0;
+      };
+    }
+  }
+}
+
